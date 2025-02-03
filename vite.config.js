8fc1f88ccea28f3f1cf36b047c15ec5d4be33674
JhonnFer <jhoncasq23@gmail.com>
2025-02-03 18:36:22 -0500

    integracion_pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'PoliTrade',
+        short_name: 'PoliTrade',
+        description: 'Aplicación PoliTrade',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/i, // Ahora también incluye webp
+
+            handler: 'CacheFirst', // Caching strategy
+            options: {
+              cacheName: 'image-cache', // Cache name for images
+              expiration: {
+                maxEntries: 200, // Maximum number of cached images
+                maxAgeSeconds: 60 * 60 * 24 * 30, // Cache for 30 days
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
